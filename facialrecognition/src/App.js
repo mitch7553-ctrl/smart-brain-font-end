04b424170ab414c8e6f51413897fb108a9a4cc9c
Mitchell Brown <mitch7553@gmail.com>
2026-01-22 09:27:08 +0800

    finishing the react front-end sedtion

---
+++ facialrecognition/src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+   <ParticlesBg  type="tadpole" num={200} bg={true} />
+      <Navigation />
+       <Logo />
+         <Rank />
+           <ImageLinkForm />
+
+    
+             
+     { 
+             /*
+             <FaceRecognition />
+             */}
+    </div>
+  );
+    }
+  }
+  
+
+export default App;
